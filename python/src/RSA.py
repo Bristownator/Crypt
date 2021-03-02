8d0f9c220e20348d1dddf668ee7e6c0db32aa640
Thomas Bristow <thomas@bristows.me.uk>
2021-03-02 21:26:30 +0000

    Adding basic studs

---
+++ python/src/RSA.py
@@ -0,0 +1,24 @@
+class RSA:
+    def __init__(self):
+        self.public_key = None
+        self.private_key = None
+
+    def generate_keys(self):
+        pass
+
+    def encrypt(self):
+        pass
+
+    def decrypt(self):
+        pass
+
+
+class RSAKeyGenerator:
+    def __init__(self):
+        pass
+
+
+class RSAKey:
+    def __init__(self, modulus, exponent):
+        self.modulus = modulus
+        self.exponent = exponent
