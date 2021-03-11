d11ba6735bb51b41cd27c7bf8134b3e47762bc40
Thomas Bristow <thomas@bristows.me.uk>
2021-03-11 20:23:47 +0000

    Adding Basic RSA encryption and decryption

--- python/src/RSA.py
+++ python/src/RSA.py
@@ -1,24 +1,108 @@
+import secrets
+
+
 class RSA:
     def __init__(self):
         self.public_key = None
         self.private_key = None
 
     def generate_keys(self):
-        pass
+        keys = RSAKeyGenerator()
+        keys.generate_keys()
+        self.public_key = RSAKey(keys.n, keys.e)
+        self.private_key = RSAKey(keys.n, keys.d)
 
-    def encrypt(self):
-        pass
+    def encrypt(self, plain_text):
+        return pow(plain_text, self.public_key.exponent, self.public_key.modulus)
 
-    def decrypt(self):
-        pass
+    def decrypt(self, cipher_text):
+        return pow(cipher_text, self.private_key.exponent, self.private_key.modulus)
 
 
 class RSAKeyGenerator:
     def __init__(self):
-        pass
+        self.p = Prime(1024).get_number()
+        self.q = Prime(1024).get_number()
+        self.n = self.p * self.q
+        self.phi_n = (self.p - 1) * (self.q - 1)
+        self.e = None
+        self.d = None
+
+    def generate_keys(self):
+        gcd = [0]
+        exponent = None
+        while gcd[0] != 1:
+            exponent = secrets.randbits(1024)
+            gcd = self.extended_euclidean(self.phi_n, exponent)
+        self.e = exponent
+        self.d = gcd[2] % self.phi_n
+
+    def extended_euclidean(self, phi_n, e):
+        large = max(phi_n, e)
+        small = min(phi_n, e)
+
+        if large % small == 0:
+            return [small, 0, 1]
+
+        output = self.extended_euclidean(small, large % small)
+
+        a = output[2]
+        b = output[1] - ((large // small) * a)
+        return [output[0], a, b]
 
 
 class RSAKey:
     def __init__(self, modulus, exponent):
         self.modulus = modulus
         self.exponent = exponent
+
+
+class Prime:
+    def __init__(self, bits):
+        self.bits = bits
+
+    def get_number(self):
+        output = None
+        prime = False
+        while not prime:
+            output = secrets.randbits(self.bits)
+            if output > 2 ** (self.bits - 1):
+                prime = self.prime(output)
+        return output
+
+    def prime(self, number):
+        b = 0
+        m = number - 1
+        remainder = "Temp"
+
+        while remainder == 0:
+            divide, remainder = divmod(m, 2)
+            if remainder == 0:
+                m = divide
+                b += 1
+
+        a = 0
+        while a % 2 == 0 & a < number:
+            a = secrets.randbits(self.bits)
+
+        z = pow(a, m, number)
+
+        if z == 1:
+            return True
+
+        if z == number - 1:
+            return False
+
+        for _ in range(b):
+            m = m * 2
+            z = z * pow(a, m, number)
+
+            if z == 1:
+                return False
+
+            if z == number - 1:
+                return True
+
+        z = z * 2
+        z = z % number
+        return z == number - 1
--- python/test/RSATest.py
+++ python/test/RSATest.py
@@ -1,9 +1,48 @@
 import unittest
 
+from RSA import RSA, RSAKeyGenerator, RSAKey, Prime
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class RSATest(unittest.TestCase):
+    def test_generate_keys(self):
+        test = RSA()
+        test.generate_keys()
+
+    def test_encrypt(self):
+        test = RSA()
+        test.generate_keys()
+        self.assertEqual(17, test.decrypt(test.encrypt(17)))
+
+
+class RSAKeyGeneratorTest(unittest.TestCase):
+    def test_generate_keys(self):
+        test = RSAKeyGenerator()
+        test.generate_keys()
+        self.assertIsNotNone(test.n)
+        self.assertIsNotNone(test.e)
+        self.assertIsNotNone(test.d)
+
+    def test_extended_euclidean(self):
+        test = RSAKeyGenerator()
+        self.assertEqual([1, 4, -55], test.extended_euclidean(179, 13))
+        self.assertEqual([1, -8695508606140362309587684798, 336816648885497786477357415267846470497], test.extended_euclidean(34732457235124123521512431323, 1345346245213587135273587124876523875235))
+
+
+class PrimeTest(unittest.TestCase):
+    def test_small_number(self):
+        for x in range(8, 128):
+            test = Prime(x).get_number()
+            self.assertGreater(test, 2 ** (x-1))
+
+    def test_big_number(self):
+        test = Prime(1024).get_number()
+        self.assertGreater(test, 2 ** 1023)
+        self.assertFalse(test % 2 == 0)
+        self.assertFalse(test % 3 == 0)
+        self.assertFalse(test % 5 == 0)
+        self.assertFalse(test % 7 == 0)
+        self.assertFalse(test % 11 == 0)
+        self.assertFalse(test % 13 == 0)
 
 
 if __name__ == '__main__':
